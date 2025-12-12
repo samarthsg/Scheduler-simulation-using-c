eda68efca4de5562898b00234a15ce2ffd97bc0a
Samarth Gupta <samarth1234gupta@gmail.com>
2025-12-12 19:01:15 +0530

    Implemented circular queue logic instead of linear queue. Introduced the priority based scheduling working on it

--- roundrobin.c
+++ roundrobin.c
@@ -1,6 +1,6 @@
 #include <stdbool.h>
 #include <stdio.h>
-#define max_size 100
+#define max_size 11
 
 typedef struct{
     int item[max_size];
@@ -10,12 +10,12 @@ typedef struct{
 
 
 bool qisfull(queue *q){
-    if(q->rear == max_size) return true;
+    if((q->rear+1) % max_size == q->front) return true;
     else return false;
 }
 
 bool qisempty(queue *q){
-    if(q->front == q->rear-1 ) return true;
+    if(q->front == -1) return true;
     else return false;
 }
 
@@ -24,10 +24,9 @@ void enqueue(queue *q, int value){
         printf("queue is full cant insert");
         return;
     }
-    else {
-        q->item[q->rear] = value;
-        q->rear++;
-    }
+    q->item[q->rear] = value;
+    q->rear = (q->rear+1)%max_size;
+    if(q->front == -1) q->front = 0;
 }
 
 void dqueue(queue *q){
@@ -35,15 +34,18 @@ void dqueue(queue *q){
         printf("Already empty Underflow!");
         return;
     }
-    else {
-        q->front++;
+    q->front = (q->front+1)%max_size;
+    if(q->front == q->rear){
+        q->front = -1;
+        q->rear = 0;
     }
 }
 
 int front_value(queue *q){
-    int temp = q->item[q->front + 1];
+    int temp = q->item[q->front];
     return temp;
 }
+
 /*
 bool check_for_completion(){
     bool result = false;
@@ -53,6 +55,7 @@ bool check_for_completion(){
 Will use this later for blocked state
 */
 struct process{
+    int priority;
     int id;
     int progress;
     bool complete;
@@ -60,37 +63,54 @@ struct process{
     bool blocked;
 };
 
+int highest_priority(queue *q, struct process p[]){
+    int highest = 0;
+    int i = q->front+1;
+    while(i < q->rear){
+        int temp = q->item[i];
+        if(p[temp - 1].priority > highest) highest = q->item[i];
+        i++;
+    }
+    return highest;
+}
+
 int main(){
     queue contain;
     contain.front = -1;
     contain.rear = 0;
 
-    struct process p[3] = {
-
-        {1, 0, false, 15, false},
-        {2, 0, false, 10, false},
-        {3, 0, false, 20, false},
+    struct process p[10] = {
+        {2, 0, 0, false, 15, false},
+        {1, 1, 0, false, 10, false},
+        {3, 2, 0, false, 20, false},
+        {3, 3, 0, false, 30, false},
+        {1, 4, 0, false, 13, false},
+        {2, 5, 0, false, 27, false},
+        {2, 6, 0, false, 28, false},
+        {1, 7, 0, false, 29, false},
+        {2, 8, 0, false, 40, false},
+        {3, 9, 0, false, 38, false},
     };
 
     int n = sizeof(p) / sizeof(p[0]);
 
     for(int i = 0; i < n; i++){
-        enqueue(&contain, p[i].id);
+        enqueue(&contain, i);
     }
 
     while(qisempty(&contain) == false){
-        int temp = front_value(&contain);
-        p[temp-1].progress++;
-        printf("Process no %d is executing: progress %d is out of %d is done\n", temp, p[temp-1].progress, p[temp-1].progress_end);
-        if(p[temp-1].progress >= p[temp-1].progress_end){
-            p[temp-1].complete = true;
+        int idx = front_value(&contain);
+        p[idx].progress++;
+        printf("Process no %d is executing: progress %d is out of %d is done\n", idx, p[idx].progress, p[idx].progress_end);
+        if(p[idx].progress >= p[idx].progress_end){
+            p[idx].complete = true;
             dqueue(&contain);
-            printf("Process no %d is done and now terminated\n", temp);
+            printf("Process no %d is done and now terminated\n", idx);
 
         }
         else{
             dqueue(&contain);
-            enqueue(&contain, temp);
+            enqueue(&contain, idx);
         }
     }
     return 0;
